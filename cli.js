f94cb0a52cc99b6f656fb00f63cf576540f2eabe
Peter deHaan <peter@deseloper.com>
2020-02-07 09:47:40 -0800

    V2.1

--- cli.js
+++ cli.js
@@ -2,13 +2,12 @@
 
 const lib = require("./lib");
 
-main()
-  .catch(err => {
-    console.error(err);
-    process.exit(1);
-  });
+main().catch(err => {
+  console.error(err);
+  process.exit(1);
+});
 
 async function main() {
-  const res = await lib.getSchedule(process.env.NOTES);
+  const res = await lib.getSchedule(process.env.NOTES, process.env.RELEASES);
   console.log(JSON.stringify(res, null, 2));
 }
--- lib.js
+++ lib.js
@@ -1,44 +1,55 @@
 const util = require("util");
 
 const axios = require("axios");
-const matter = require('gray-matter');
-const {DateTime} = require("luxon");
-const md = require('markdown-it')();
+const matter = require("gray-matter");
+const { DateTime } = require("luxon");
 const $nodeVersionData = require("node-version-data");
+const md = require("markdown-it")();
 
 const SCHEDULE_URL =
   "https://raw.githubusercontent.com/nodejs/Release/master/schedule.json";
 const nodeVersionData = util.promisify($nodeVersionData);
 
-
 module.exports = {
-  getSchedule
+  getSchedule,
+  fetchReleaseNotes,
+  relativeDate,
+  SCHEDULE_URL,
+  nodeVersionData
 };
 
-async function getSchedule(includeBlogPost=false) {
-  const versionData = await nodeVersionData();
+async function getSchedule(includeBlogPost = false, includeReleases = false) {
+  let versionData = await nodeVersionData();
+  versionData = versionData.map(({ files, ...rest }) => rest);
 
   const now = Date.now();
   const res = await axios.get(SCHEDULE_URL);
   const versions = Object.entries(res.data)
     .map(([version, item]) => {
       item.start = new Date(item.start);
-      item.start2 = relativeDate(item.start);
+      // item.start2 = relativeDate(item.start);
       item.end = new Date(item.end);
-      item.end2 = relativeDate(item.end);
+      // item.end2 = relativeDate(item.end);
       if (item.maintenance) {
         item.maintenance = new Date(item.maintenance);
-        item.maintenance2 = relativeDate(item.maintenance);
+        // item.maintenance2 = relativeDate(item.maintenance);
       }
       if (item.lts) {
         item.lts = new Date(item.lts);
-        item.lts2 = relativeDate(item.lts);
+        // item.lts2 = relativeDate(item.lts);
       }
-      item.latest = versionData.find(release => release.version.split(".")[0] === version);
+      item.latest = versionData.find(release =>
+        release.version.startsWith(version)
+      );
       if (item.latest) {
-        delete item.latest.files;
         item.latest.date = new Date(item.latest.date);
-        item.latest.date2 = relativeDate(item.latest.date);
+        // item.latest.date2 = relativeDate(item.latest.date);
+      }
+
+      if (includeReleases) {
+        item.releases = versionData.filter(release =>
+          release.version.startsWith(version)
+        );
       }
       return Object.assign({ version }, item);
     })
@@ -49,14 +60,17 @@ async function getSchedule(includeBlogPost=false) {
     // Fetch the blog post for any active (non-filtered) releases.
     for (const release of versions) {
       release.latest.notes = await fetchReleaseNotes(release.latest.version);
-      release.latest.notes.html = md.render(release.latest.notes.content).replace(/&lt;br&gt;/g, "<br/>");
+      release.latest.notes.html = md
+        .render(release.latest.notes.content)
+        .replace(/&lt;br&gt;/g, "<br/>");
     }
   }
   return versions;
 }
 
-function relativeDate(dateObj) {
-  return DateTime.fromJSDate(dateObj).toRelative()
+function relativeDate(date) {
+  date = new Date(date);
+  return DateTime.fromJSDate(date).toRelative();
 }
 
 async function fetchReleaseNotes(version) {
